27f0af175700d05ee91000dd9d1e9b5b39a788e6
haz3d <125324123+haz3d@users.noreply.github.com>
2023-02-17 14:31:04 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
-import { useState } from 'react';
-import { auth } from '../firebase';
+// pages/index.tsx
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { auth } from "../firebase";
 
-const IndexPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const Login = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const handleLogin = async () => {
     try {
       await auth.signInWithEmailAndPassword(email, password);
       // Redirect the user to the To-Do List page
-    } catch (error) {
+      router.push("/todo");
+    } catch (error: any) { // add a type annotation to the 'error' variable
       console.log(error.message);
     }
   };
@@ -18,29 +21,19 @@ const IndexPage = () => {
   return (
     <div>
       <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-        <br />
-        <button type="submit">Log in</button>
-      </form>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Log In</button>
     </div>
   );
 };
 
-export default IndexPage;
+export default Login;
