917c6e93b741d850732642c27f20eb47c6a9524c
Lumine <120704613+ruihq@users.noreply.github.com>
2023-02-17 14:35:39 +0800

    I fix it :)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-module.exports = {
-  // remove 'images' from the experimental property
+/** 
+ * Next.js configuration file
+ */
+
+// Import the Next.js configuration object
+const nextConfig = {
+  // Enable strict mode for React
+  reactStrictMode: true,
+
+  // Add experimental features
+  // NOTE: Experimental features are not covered by semver, and may cause unexpected or broken application behavior. Use at your own risk.
   experimental: {
-    swcLoader: true
-  }
-};
+    // Enable the 'images' feature
+    images: true,
+
+    // Enable the 'swcLoader' feature
+    swcLoader: true,
+  },
+}
+
+// Export the configuration object
+module.exports = nextConfig
