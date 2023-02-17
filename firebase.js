c5a20c35bed9c14539a5abdafac12584030b92e2
haz3d <125324123+haz3d@users.noreply.github.com>
2023-02-17 14:22:15 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,7 +2,15 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  // Add your Firebase configuration here
+  const firebaseConfig = {
+  apiKey: "AIzaSyB4Qikd16TtZA_KHJciR6OL-gprRfhi0p8",
+  authDomain: "todo-a2b88.firebaseapp.com",
+  projectId: "todo-a2b88",
+  storageBucket: "todo-a2b88.appspot.com",
+  messagingSenderId: "439147264246",
+  appId: "1:439147264246:web:7c67e263799aa75dcd94d7",
+  measurementId: "G-WS2BHH955V"
+};
 };
 
 if (!firebase.apps.length) {
